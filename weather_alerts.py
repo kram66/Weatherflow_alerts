cff4aed41fced4baae162c7e173720a8197af8ad
kram66 <34220061+kram66@users.noreply.github.com>
2021-07-09 14:02:17 +1000

    Update weather_alerts.py

--- weather_alerts.py
+++ weather_alerts.py
@@ -6,14 +6,16 @@ import requests as req
 import http.client, urllib
 import time
 
-personal_token = "your personal token here"
-tempest_ID = "your tempest id here"
-station_id = "your station id here"
-push_token = "your push token here"
-user = "your user id here"
+personal_token = "[token here]"
+mytoken = "[token here]"
+tempest_ID = '[token here]'
+station_id = "[token here]"
+push_token = "[token here]"
+user = "[token here]"
 
 """
 The UV Index divides UV radiation levels into:
+
 low (1-2)
 moderate (3-5)
 high (6-7)
@@ -22,31 +24,33 @@ extreme (11 and above).
 """
 
 		
-#Lets setup some defaults for our alerts. Setup your own values here
-a_WW		 		= 15		#Alert if we get wind more than 15kms
-a_RW		 		= 5		#Alert if we get 5mm or more of rain
-a_TH 				= 30		#Alert if the temp gets above 30	
-a_TL		 		= 1		#Alert if the temp gets below 1
-a_SC		 		= 5		#Alert if we get more than 5 lightning strikes in the last hour
-a_WC 				= 5		#Alert if the windchill is 5 or lower
-a_BV	 			= 2 		#Alert if battery level drops
-a_UV 				= 6  		#UV index alert
-a_RA		 		= 1380		#This is a normal value
-#a_HH			 	= 95		#High and low values for humidity
-a_HL		 		= 20		#Low humidity values
-
+#Lets setup some defaults for our alerts
+a_WW		 		= 15	#Alert if we get wind more than 15kms
+a_RW		 		= 6	#Alert if we get 5mm or more of rain
+a_TH 				= 30	#Alert if the temp gets above 30	
+a_TL		 		= 2 	#Alert if the temp gets below 1
+a_SC		 		= 5	#Alert if we get more than 5 lightning strikes in the last hour
+a_WC 				= 2	#Alert if the windchill is 5 or lower
+a_BV	 			= 2 	#Alert if battery level drops
+a_UV 				= 6 	#UV index alert 6 is considered high
+a_RA		 		= 1380	#This is a normal value
+a_HL		 		= 20	#Low humidity values
 
 #Lists
 NumonicList	= ['WW:','RW:','WC:','BV:','HH:','CW:','RA:','HL:','UV:']	
-MsgList = ['Wind Warning:','Rain Warning:','Wind Chill Warning:','Battery Voltage Warning:','High Heat Warning:','Cold Warning:','Radiation Warning:','Low Humidity Warning:', 'High UV Index']	
+MsgList = ['Wind:','Rain:','Wind Chill:','Battery Voltage:','High Heat:','Cold:','Radiation:','Low Humidity:', 'High UV:']	
 Warning = [' km',' mm',' C',' V',' C',' C',' W/m^2',' %',' I']	
 Thresholds = [a_WW, a_RW, a_WC, a_BV, a_TH, a_TL, a_RA, a_HL,a_UV]
 Operator = ['GT','GT','LT','LT','GT','LT','GT','LT','GT']
-thislist = []
-messageList = []
+MyList = []
+
+
+#variables
 msg = ""
 COUNT = 0
+counter = 0
 status = 'CLEAR'
+interval = 10
 
 def nonzero(value):
     if value == None:
@@ -71,55 +75,64 @@ def SendNotification(Title):
 		files = {
 			"attachment": ("warning.jpg", open(r"d:\python\weather\warning.jpg", "rb"), "image/jpeg")
 		})
-counter = 0
-
+	
+		
 def increment():
 	global COUNT
 	COUNT = COUNT +1
 	return COUNT
 	
 def getAlert(AlertList, Thresholds, Warning, MsgList, NumonicList, operator):
+	
 	#Lets check if we have a warning
+	MyList = [MsgList]
 	if operator == "GT":
 		if AlertList >= Thresholds:
-			status = "WARNING"
-			#Ok, We have breached our threshold add to the counter
 			counter = increment()
-			newstring= NumonicList + str(AlertList)
-			#Now lets build the list. If the counter is 1 it's the first time so send the alert
-			if counter == 1:
-				thislist.append(newstring)
-				messageList.append(status)
-				messageList.append(MsgList + " " + str(AlertList) + Warning)
-				messageList.append(counter)
-			elif counter > 1:
-				messageList.pop()
-				messageList.append(counter)
-		elif AlertList <= Thresholds:
+			if "WARNING" not in MyList:
+				MyList.append("WARNING")
+				MyList.append(AlertList)
+				MyList.append(Thresholds)
+				MyList.append(Warning)
+				MyList.append(NumonicList)
+				MyList.append(counter)
+				status = "WARNING"
+			elif "WARNING" not in MyList and counter >1:
+				MyList.pop()
+				MyList.append(counter)
+				status = "WARNING"
+		else:
+			MyList = []
 			status = "CLEAR"
 	else:
 		if AlertList <= Thresholds:
-			status = "WARNING"
-			#Ok, We have breached our threshold add to the counter
 			counter = increment()
-			newstring= NumonicList + str(AlertList)
-			#Now lets build the list. If the counter is 1 it's the first time so send the alert
-			if counter == 1:
-				thislist.append(newstring)
-				messageList.append(status)
-				messageList.append(MsgList + " " + str(AlertList) + Warning)
-				messageList.append(counter)
-			elif counter > 1:
-				messageList.pop()
-				messageList.append(counter)
-		elif AlertList >= Thresholds:
+			if "WARNING" not in MyList:
+				MyList.append("WARNING")
+				MyList.append(AlertList)
+				MyList.append(Thresholds)
+				MyList.append(Warning)
+				MyList.append(NumonicList)
+				MyList.append(counter)
+				status = "WARNING"
+			elif "WARNING" not in MyList and counter >1:
+				status = "WARNING"	
+				counter = increment()
+				MyList.pop()
+				MyList.append(counter)
+		else:
+			MyList = []
 			status = "CLEAR"
-	output = "{:<19}Current: {:<12} Thresholds: {:<12} Status: {:<12}".format(MsgList, AlertList, Thresholds, status)
-	print(output)
-	return messageList		
+			
+	myval = "{:<19}Current: {:<12} Thresholds: {:<12} Status: {:<12}".format(MsgList, AlertList, Thresholds, status)
+	print(myval)
+	return MyList		
+	
+
+
 
 def opensocket():
-	try:
+	#try:
 		#print('Opening Websocket connection...')
 		websock = create_connection('wss://ws.weatherflow.com/swd/data?api_key=' + personal_token)
 		temp_rs =  websock.recv()
@@ -152,23 +165,21 @@ def opensocket():
 
 		for a, b, c, d, e, f in zip(AlertList, MsgList, Warning, Thresholds, Operator, NumonicList):
 			msg = getAlert(a, d, c, b, f, e)
-
-
-		#Now lets loop through the list and see what alerts we need to send.
-		#We have a warning
-		if len(msg) > 0:
-			#First, lets check if the message has already been sent
-			messageSent = msg[-1]
-			if messageSent > 1:
-				print('message is already sent')
+			if len(msg) > 0:
+				msgcount = msg[-1]
+				print('count=', msgcount)
+				if "WARNING" in msg and msgcount == 1:
+					WARNING = "WARNING"
+					print('We have our first warning.. sending an alert')
+					messagestring=str(msg[0]), ' ', str(msg[2]), str(msg[4]), ' - Threshold: ', str(msg[3]) 
+					SendNotification(listToString(messagestring))
+				elif msgcount > 1:
+					print('already sent the message')
 			else:
-				print('send the alert')
-				SendNotification(listToString(msg[1]))
-	except:
-		pass
+				WARNING = "CLEAR"
+
 	
 while True:
 	opensocket()
 	print('sleeping')
-	time.sleep(10)
-	
+	time.sleep(interval)
